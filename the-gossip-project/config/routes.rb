6a6d3b3cb8f22ef60fd94385042eb3672221b441
QuentinTHP <quentin.clere.pro@gmail.com>
2019-02-05 02:06:33 +0100

    Ajout des navbar et de la page Welcome

--- the-gossip-project/config/routes.rb
+++ the-gossip-project/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'welcome/:first_name', to: 'welcome#first_name'
   get 'static_pages/team'
   get 'static_pages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
